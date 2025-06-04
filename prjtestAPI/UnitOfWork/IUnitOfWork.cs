a0cd9f44229c1e517c639a737ccc7e9f67025dd3
Eason0311 <s0311s0311@gmail.com>
2025-06-04 17:09:47 +0800

    新增UoW的多段儲存交易模式

--- prjtestAPI/UnitOfWork/IUnitOfWork.cs
+++ prjtestAPI/UnitOfWork/IUnitOfWork.cs
@@ -6,4 +6,5 @@ public interface IUnitOfWork
     IUserRepository Users { get; }
     IRefreshTokenRepository RefreshTokens { get; }
     Task<int> CompleteAsync();
+    Task ExecuteTransactionAsync(Func<Task> action);
 }
--- prjtestAPI/UnitOfWork/UnitOfWork.cs
+++ prjtestAPI/UnitOfWork/UnitOfWork.cs
@@ -5,19 +5,39 @@ using prjtestAPI.Repositories.Interfaces;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly TestApiContext _context;
-    private IUserRepository? _users;
-    private IRefreshTokenRepository? _refreshTokens;
+    private readonly IUserRepository _users;
+    private readonly IRefreshTokenRepository _refreshTokens;
 
-    public UnitOfWork(TestApiContext context)
+    public UnitOfWork(
+        TestApiContext context,
+        IUserRepository users,
+        IRefreshTokenRepository refreshTokens)
     {
         _context = context;
+        _users = users;
+        _refreshTokens = refreshTokens;
     }
 
-    public IUserRepository Users => _users ??= new UserRepository(_context);
-    public IRefreshTokenRepository RefreshTokens => _refreshTokens ??= new RefreshTokenRepository(_context);
+    public IUserRepository Users => _users;
+    public IRefreshTokenRepository RefreshTokens => _refreshTokens;
 
     public async Task<int> CompleteAsync()
     {
         return await _context.SaveChangesAsync();
+    }
+
+    public async Task ExecuteTransactionAsync(Func<Task> action)
+    {
+        await using var tx = await _context.Database.BeginTransactionAsync();
+        try
+        {
+            await action();
+            await tx.CommitAsync();
+        }
+        catch
+        {
+            await tx.RollbackAsync();
+            throw;
+        }
     }
 }
